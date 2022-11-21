b05ff6a70c5a8a76d0ea3c75d10611753143741d
Alexander Bessonov <bessonovUSA@gmail.com>
2022-11-20 23:14:00 -0600

    Heap Class with insertion Created

--- dsa-09-Heap/src/main/java/MyHeap.java
+++ dsa-09-Heap/src/main/java/MyHeap.java
@@ -2,4 +2,52 @@ import java.util.NoSuchElementException;
 
 public class MyHeap {
 
+    int[] items;
+    int size;
+
+    public MyHeap(int capacity) {
+        this.items = new int [capacity];
+        this.size=0;
+    }
+
+    public int peek(){
+        if (size==0) throw new NoSuchElementException();
+        return items[0];
+    }
+    public void insert(int value){
+        if (size==items.length) throw new IllegalStateException();
+        else{
+           items[size++]=value;
+           bubbleUp();
+        }
+    }
+    public void bubbleUp(){
+        //calculate start point / index
+        int index=size-1;
+        // while index > 0 %% parent is less than child continue...
+        // 1. swap child and parent
+        //calculate next index = parent index
+        while (index > 0 && items[index] > items[parentIndex(index)]){
+           //swap
+            swap(index, parentIndex(index));
+           //assign index with the parent index
+           index = parentIndex(index); // like current = current.next
+        }
+    }
+    public void swap(int first, int second){
+        int temp = items[first];
+        items[first] = items[second];
+        items[second] = temp;
+    }
+    public int parentIndex(int index){
+
+       return (index-1)/2;
+    }
+    public void printHeap(){
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i]+", ");
+        }
+    }
+
 }
