ed3027d4551f2f7c9f51aaa2e27fafa149924878
Alexander Bessonov <bessonovUSA@gmail.com>
2022-12-16 11:18:50 -0600

    dsa - 10 - Search Start

---
+++ dsa-10-Search/src/test/java/com/example/dsa10search/Dsa10SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsa10search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsa10SearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
